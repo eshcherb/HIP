a21142396693737a47cbe11444af9bcd904763a3
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2018-06-02 18:00:38 -0400

    Add missing __device__ for __builtin_amdgcn_atomic_inc/dec

--- include/hip/hcc_detail/hip_atomic.h
+++ include/hip/hcc_detail/hip_atomic.h
@@ -176,7 +176,7 @@ __device__
 inline
 unsigned int atomicInc(unsigned int* address, unsigned int val)
 {
-    extern unsigned int __builtin_amdgcn_atomic_inc(
+    extern __device__ unsigned int __builtin_amdgcn_atomic_inc(
         unsigned int*,
         unsigned int,
         unsigned int,
@@ -191,7 +191,7 @@ __device__
 inline
 unsigned int atomicDec(unsigned int* address, unsigned int val)
 {
-    extern unsigned int __builtin_amdgcn_atomic_dec(
+    extern __device__ unsigned int __builtin_amdgcn_atomic_dec(
         unsigned int*,
         unsigned int,
         unsigned int,
